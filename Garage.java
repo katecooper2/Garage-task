0951bd77efc5f969a5994e8d8c2d0b44c3b73777
katee.cooper2@gmail.com <kate cooper>
2021-11-12 14:51:32 +0000

    garage.java

---
+++ Garage.java
@@ -0,0 +1,58 @@
+package qa.Garage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import qa.Vehicle.Bicycle;
+import qa.Vehicle.Car;
+import qa.Vehicle.Motorbike;
+import qa.Vehicle.Vehicle;
+
+public class Garage {
+	
+	public static void main(String[] args) {
+		
+		
+		List<String> Vehicle = new ArrayList<String>();
+		
+		Vehicle.add("Car");
+		Vehicle.add("Motorbike");
+		Vehicle.add("Bicycle");
+		
+		for(int i=0; i<Vehicle.size(); i++) {
+			System.out.println(Vehicle.get(i));
+			
+			
+		}
+		
+		
+	}
+
+	public static void addVehicle(Car car) {
+	
+	}
+
+	public static void addVehicle(Motorbike motorbike) {
+	
+	}
+
+	public static void addVehicle(Bicycle bicycle) {
+		
+	}
+
+
+
+	private Vehicle[] Vehicle;
+
+	
+	
+	public void Bill() {
+		int totalBill;
+		for(Vehicle vehicle : Vehicle) {
+			totalBill +}
+	}
+}
+
+}
+	
+
